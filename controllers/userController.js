2bd7b1942f8b75f8d93042dd865c865672b59bf6
joel-foo <joelfootaimeng@gmail.com>
2023-10-13 00:59:00 +0800

    edit

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,17 +3,17 @@ const userService = require('../service/userService');
 
 module.exports.post = tryCatch(async (req, res) => {
   const dataToSave = await userService.createUser(req.body);
-  res.status(200).json(dataToSave);
+  return res.status(200).json(dataToSave);
 });
 
 module.exports.getAll = tryCatch(async (req, res) => {
   const data = await userService.getAllUsers();
-  res.status(200).json(data);
+  return res.status(200).json(data);
 });
 
 module.exports.getOne = tryCatch(async (req, res) => {
   const data = await userService.getUserById(req.params);
-  res.status(200).json(data);
+  return res.status(200).json(data);
 });
 
 module.exports.update = tryCatch(async (req, res) => {
@@ -21,10 +21,10 @@ module.exports.update = tryCatch(async (req, res) => {
     id: req.params.id,
     updateInfo: req.body,
   });
-  res.status(200).send(data);
+  return res.status(200).send(data);
 });
 
 module.exports.delete = tryCatch(async (req, res) => {
   const data = await userService.deleteUser(req.params);
-  res.status(200).send(`Document with ${data.name} has been deleted..`);
+  return res.status(200).send(`Document with ${data.name} has been deleted..`);
 });
